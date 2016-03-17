1a2e0eb74318d7e4200f3cfc5de22d88e834ac48
Paige Hetherington <herbmama@gmail.com>
2016-03-17 15:53:15 -0400

    Add CRUD and search routes

--- src/main/java/com/theironyard/entities/HappyHour.java
+++ src/main/java/com/theironyard/entities/HappyHour.java
@@ -30,7 +30,26 @@ public class HappyHour {
     String endTime;
 
     @Column(nullable = false)
-    ArrayList<String> days;
+    boolean onMonday = false;
+
+    @Column(nullable = false)
+    boolean onTuesday = false;
+
+    @Column(nullable = false)
+    boolean onWednesday = false;
+
+    @Column(nullable = false)
+    boolean onThursday = false;
+
+    @Column(nullable = false)
+    boolean onFriday = false;
+
+    @Column(nullable = false)
+    boolean onSaturday = false;
+
+    @Column(nullable = false)
+    boolean onSunday = false;
+
 
     @Column(nullable = false)
     List<String> specials;
@@ -39,15 +58,22 @@ public class HappyHour {
     String image;
 
     public HappyHour() {
+
     }
 
-    public HappyHour(String name, String address, String city, String startTime, String endTime, ArrayList<String> days, List<String> specials, String image) {
+    public HappyHour(String name, String address, String city, String startTime, String endTime, boolean onMonday, boolean onTuesday, boolean onWednesday, boolean onThursday, boolean onFriday, boolean onSaturday, boolean onSunday, List<String> specials, String image) {
         this.name = name;
         this.address = address;
         this.city = city;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.days = days;
+        this.onMonday = onMonday;
+        this.onTuesday = onTuesday;
+        this.onWednesday = onWednesday;
+        this.onThursday = onThursday;
+        this.onFriday = onFriday;
+        this.onSaturday = onSaturday;
+        this.onSunday = onSunday;
         this.specials = specials;
         this.image = image;
     }
@@ -100,12 +126,60 @@ public class HappyHour {
         this.endTime = endTime;
     }
 
-    public ArrayList<String> getDays() {
-        return days;
+    public boolean isOnMonday() {
+        return onMonday;
     }
 
-    public void setDays(ArrayList<String> days) {
-        this.days = days;
+    public void setOnMonday(boolean onMonday) {
+        this.onMonday = onMonday;
+    }
+
+    public boolean isOnTuesday() {
+        return onTuesday;
+    }
+
+    public void setOnTuesday(boolean onTuesday) {
+        this.onTuesday = onTuesday;
+    }
+
+    public boolean isOnWednesday() {
+        return onWednesday;
+    }
+
+    public void setOnWednesday(boolean onWednesday) {
+        this.onWednesday = onWednesday;
+    }
+
+    public boolean isOnThursday() {
+        return onThursday;
+    }
+
+    public void setOnThursday(boolean onThursday) {
+        this.onThursday = onThursday;
+    }
+
+    public boolean isOnFriday() {
+        return onFriday;
+    }
+
+    public void setOnFriday(boolean onFriday) {
+        this.onFriday = onFriday;
+    }
+
+    public boolean isOnSaturday() {
+        return onSaturday;
+    }
+
+    public void setOnSaturday(boolean onSaturday) {
+        this.onSaturday = onSaturday;
+    }
+
+    public boolean isOnSunday() {
+        return onSunday;
+    }
+
+    public void setOnSunday(boolean onSunday) {
+        this.onSunday = onSunday;
     }
 
     public List<String> getSpecials() {
